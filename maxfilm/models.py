a8379b6ad9354d609be105a988cd7e263676abce
jchierro <jchierro@hotmail.com>
2016-04-05 19:57:21 +0200

    Added Userprofile

--- maxfilm/models.py
+++ maxfilm/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """Class UserProfile"""
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    birth_date = models.DateField(blank=False, null=False)
+    photo = models.ImageField(upload_to='profiles', blank=False, null=False)
+
+    def __str__(self):
+        return self.user.username
+
+    def __unicode__(self):
+        return self.user.username
